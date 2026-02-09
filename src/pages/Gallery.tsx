cfc2a2407f5c0a3cc297dea807ed6bae6b2cc2cc
Dickson Esamai <mukweesamai@gmail.com>
2026-02-09 07:27:44 +0300

    image overlay title removal

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -102,11 +102,11 @@ const Gallery = () => {
                     <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500" />
 
                     {/* Title overlay on hover */}
-                    <div className="absolute inset-0 flex items-end p-6 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                    {/* <div className="absolute inset-0 flex items-end p-6 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                       <h3 className="text-white text-xl font-bold drop-shadow-lg">
                         {image.title}
                       </h3>
-                    </div>
+                    </div> */}
                   </div>
 
                   {/* Caption / description */}
